7d22b523cb85dee35de20667aea94bf14f8cdd7e
Rob Taylor <Robert_taylor_@hotmail.com>
2014-01-28 21:25:16 +0000

    removed warnings

--- src/main/java/sample/PopulateMessages.java
+++ src/main/java/sample/PopulateMessages.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
 
 import sample.entity.Message;
 
 public class PopulateMessages {
    public static void main(String[] args) {
          SessionFactory factory =
-            new AnnotationConfiguration().configure().buildSessionFactory();
+            new Configuration().configure().buildSessionFactory();
          Session session = factory.openSession();
          session.beginTransaction();
 
@@ -22,3 +22,4 @@ public class PopulateMessages {
          session.close();
    }
 }
+
